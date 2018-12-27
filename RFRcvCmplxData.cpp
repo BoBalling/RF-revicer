d581847b59ba2d28482d7f91a05df2ec61cbb675
Bo Balling <36796187+BoBalling@users.noreply.github.com>
2018-12-27 08:53:09 +0100

    Add files via upload

--- RFRcvCmplxData.cpp
+++ RFRcvCmplxData.cpp
@@ -43,21 +43,26 @@ unsigned char t1, t4, stationCode = 0;
 unsigned short t2, t3 = 0;
 float temp, humid, batt = 0;
 
+char buffer[MAXBUF];
 
+struct Data data;
+int responseCode = 0;
 
-// test data
-//unsigned int value = 4294961097;   //15-1023-999-201
-//unsigned int value = 4294967295;   //15-1023-1023-255
-//unsigned int value = 4026531841;   //15-0-0-1
+// test data3-999-201
+//unsigned int value = 322055266;   
+//unsigned int value = 322317154;   
+//unsigned int value = 321793378;   
+//unsigned int value = 321793634;
 
+size_t function_pt(char *ptr, size_t size, size_t nmemb, void *stream) {
+    printf("%s\n", ptr);
+    responseCode = atoi(ptr);
+    return size * nmemb;
 }
 
 int main(int argc, char *argv[]) {
 
     float startTime = clock();
-
-
-   
 
     // we get lots of duplicates in the same transmission so we'll remember
     // the last value received so we don't react twice to the same value
@@ -111,18 +116,6 @@ int main(int argc, char *argv[]) {
               data.humid = 0.0;
               data.batt = 0.0;
 
-              // if t2 and t3 are 0, it's motion sensor data; otherwise is temp/humid/batt
-              if(t2 == 0 && t3 ==0) {
-                  isMotion = true;
-                  // real values
-                  stationCode = t1;    // numeric code 0-15
-                  motion = t4;         // 0=off,1=on
-                  printf("single values: %u-%u\n", stationCode, motion);
-                  data.stationCode = stationCode;
-                  data.motion = motion;
-                  postData();
-              } else {
-                  isMotion = false;
                   // real values
                   stationCode = t1;    // numeric code 0-15
                   temp = t2 / 10.0;    // F
@@ -133,8 +126,8 @@ int main(int argc, char *argv[]) {
                   data.temp = temp;
                   data.humid = humid;
                   data.batt = batt;
-                  postData();
-              }
+                  
+              
 
               // store the new value as lastValue
               lastValue = value;
@@ -144,115 +137,5 @@ int main(int argc, char *argv[]) {
         mySwitch.resetAvailable();
       }
     }
-
-    if(readyToSendToServer) {
-        /* always cleanup */
-        curl_easy_cleanup(curl);
-    }
-//    sqlite3_finalize(dbRes);
-    sqlite3_close(dbConn);
-
     exit(0);
-}
-
-// data doesn't get modified so pass by value
-void postData()
-{
-    // able to use the same curl because it is not dependent on a url
-    postSparkfun();
-    postDweet();
-    postThingspeak();
-}
-
-void postSparkfun() {
-    buffer[0] = '\0';
-    // post to data.sparkfun.com
-    if(isMotion) {
-        // send motion sensor data
-        // http://data.sparkfun.com/input/[publicKey]?private_key=[privateKey]&station=[value]&motion=[value]
-        sprintf(buffer,
-            "%s/input/%s?private_key=%s&%s=%u&%s=%u", sparkfunServerUrl, publicKeyPIR, privateKeyPIR,
-            stationKey, data.stationCode, motionKey, data.motion);
-    } else {
-        // send temp/humid/batt sensor data
-        // http://data.sparkfun.com/input/[publicKey]?private_key=[privateKey]&station=[value]&humidity=[value]&temp=[value]&voltage=[value]
-        sprintf(buffer,
-            "%s/input/%s?private_key=%s&%s=%u&%s=%.1f&%s=%.1f&%s=%.1f", sparkfunServerUrl, publicKeyDHT, privateKeyDHT,
-            stationKey, data.stationCode, humidityKey, data.humid, tempKey, data.temp, voltageKey, data.batt);
-    }
-    doPost(buffer);
-}
-
-void postDweet() {
-    buffer[0] = '\0';
-    // post to dweet.io
-    if(isMotion) {
-        // send motion sensor data
-        // https://dweet.io/dweet/for/my-thing-name?station=[value]&motion=[value]
-        sprintf(buffer,
-            "%s%s?%s=%u&%s=%u", dweetServerUrl, dweetPIRName,
-            stationKey, data.stationCode, motionKey, data.motion);
-    } else {
-        // send temp/humid/batt sensor data
-        // https://dweet.io/dweet/for/my-thing-name?station=[value]&humidity=[value]&temp=[value]&voltage=[value]
-        sprintf(buffer,
-            "%s%s?%s=%u&%s=%.1f&%s=%.1f&%s=%.1f", dweetServerUrl, dweetDHTName,
-            stationKey, data.stationCode, humidityKey, data.humid, tempKey, data.temp, voltageKey, data.batt);
-    }
-    doPost(buffer);
-}
-
-void postThingspeak() {
-    buffer[0] = '\0';
-    // post to api.thingspeak.com
-    if(isMotion) {
-        // send motion sensor data
-        // http://api.thingspeak.com/update?api_key=[privateKey]&field1=[value]&field2=[value]
-        sprintf(buffer,
-            "%s/update?api_key=%s&%s=%u&%s=%u", thingspeakServerUrl, tsPrivateKeyPIR,
-            tsStationKey, data.stationCode, tsMotionKey, data.motion);
-    } else {
-        // send temp/humid/batt sensor data
-        // http://api.thingspeak.com/update?api_key=[privateKey]&field1=[value]&field2=[value]&field3=[value]&field4=[value]
-        sprintf(buffer,
-            "%s/update?api_key=%s&%s=%u&%s=%.1f&%s=%.1f&%s=%.1f", thingspeakServerUrl, tsPrivateKeyDHT,
-            tsStationKey, data.stationCode, tsHumidityKey, data.humid, tsTempKey, data.temp, tsVoltageKey, data.batt);
-    }
-    // write to db
-    doPost(buffer);
-    unsigned short posted = 1;
-    if(responseCode == 0) {
-        // if we got 0 in the responseCode, the post was not accepted (less than 15s): posted = 0
-        posted = 0;
-    }
-    // post to db (only when posting to thinkspeak for now)
-    postDb(posted);
-}
-
-void doPost(char buffer[]) {
-    printf("\n%s\n", buffer);
-    if(readyToSendToServer) {
-        curl_easy_setopt(curl, CURLOPT_URL, buffer);
-
-        /* Perform the request, res will get the return code */
-        res = curl_easy_perform(curl);
-        /* Check for errors */
-        if(res != CURLE_OK) {
-            fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
-        }
-    }
-}
-
-void postDb(int posted) {
-    dbBuffer[0] = '\0';
-    if(isMotion) {
-        sprintf(dbBuffer, sqlInsertPIR, data.stationCode, motion, posted);
-    } else {
-        sprintf(dbBuffer, sqlInsertDHT, data.stationCode, data.temp, data.humid, data.batt, posted);
-    }
-    puts(dbBuffer);
-    dbError = sqlite3_exec(dbConn, dbBuffer, 0, 0, 0);
-    if (dbError) {
-        puts("Can not insert into database");
-    }
 }
